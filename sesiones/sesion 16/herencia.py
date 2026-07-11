6d3ecc56104afc5aa98c5ee3e1022d4ba478a94e
AVOIID-ME <fergomez0310@gmail.com>
2026-07-11 01:46:41 +0000

    herencia

---
+++ sesiones/sesion 16/herencia.py
@@ -0,0 +1,52 @@
+class Abuelo:
+    def __init__(self, name):
+        self.nombreAbuelo = name
+
+    def saludo(self):
+        print(self.nombreAbuelo)
+
+class Padre(Abuelo):
+    def __init__(self, nombre, abuelo):
+        super().__init__(self,abuelo)
+        self.nombrePadre = nombre
+
+    def saludo(self):
+        print(self.nombrePadre)
+
+class Madre(Abuelo):
+    def __init__(self, nombre, abuelo):
+        super().__init__(self,abuelo)
+        self.nombreMadre = nombre
+
+    def saludo(self):
+        print(self.nombreMadre)
+
+class Hijo(Padre, Madre):
+    def __init__(self, nombre, padre, madre, abuelo):
+        padre.__init__(self,padre, abuelo)
+        madre.__init__(self,madre, abuelo)
+        self.nombreHijo = nombre
+
+    def saludo(self):
+        print(self.nombreHijo)
+
+    def identificarPadres(self):
+        print(f"Mi padre es {self.nombrePadre} y mi madre es {self.nombreMadre}.")
+        print(f"Mi abuelo es {self.nombreAbuelo}.")
+
+if __name__ == '__main__':
+    abuelo1 = Abuelo("Sergio")
+    padre1 = Padre("Fernando", abuelo1.nombreAbuelo)
+    madre1 = Madre("Yessica", abuelo1.nombreAbuelo)
+    hijo1 = Hijo("Fer", padre1.nombrePadre, madre1.nombreMadre, abuelo1.nombreAbuelo)
+
+    abuelo1.saludo()
+    padre1.saludo()
+    madre1.saludo()
+    hijo1.saludo()
+    hijo1.identificarPadres()
+
+
+
+
+    
